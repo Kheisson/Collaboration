107962e126af6fb40406fe252fc73687759569c6
Kson <igor.kheison@gmail.com>
2021-03-08 15:59:37 +0200

    Room Camera Control

---
+++ Assets/Scripts/CameraShifter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShifter : MonoBehaviour
+{
+    public GameObject virtualCamera;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            virtualCamera.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            virtualCamera.SetActive(false);
+    }
+}
